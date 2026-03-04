db222add41dd409b9498641fbb953b4e7d4ef108
shankar117512 <shankar117512@gmail.com>
2026-03-04 18:47:13 +0000

    base

--- core/settings/base.py
+++ core/settings/base.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CSRF_TRUSTED_ORIGINS = ["https://acceptable-spontaneity-dev.up.railway.app"]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
