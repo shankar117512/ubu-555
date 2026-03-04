a40d3561ed1db3f0216e192ad0303a5818bdb58f
shankar117512 <shankar117512@gmail.com>
2026-03-04 18:39:03 +0000

    add

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -1,4 +1,4 @@
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["https://acceptable-spontaneity-dev.up.railway.app"]
+ALLOWED_HOSTS = ["https://acceptable-spontaneity-dev.up.railway.app", "acceptable-spontaneity-dev.up.railway.app"]
