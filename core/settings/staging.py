13210bdbf17ffac6d6b2660cccf0855591522755
shankar117512 <shankar117512@gmail.com>
2026-03-06 05:43:27 +0000

    settings added

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -4,4 +4,5 @@ DEBUG = False
 ALLOWED_HOSTS = ["cooperative-enjoyment-staging.up.railway.app", "https://cooperative-enjoyment-staging.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://cooperative-enjoyment-staging.up.railway.app"]
 
-
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
