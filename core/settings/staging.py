e1c5e3ac3c9ed5b09a6b2310537de9418bfd9d7f
shankar117512 <shankar117512@gmail.com>
2026-03-06 05:19:18 +0000

    add

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -4,6 +4,4 @@ DEBUG = False
 ALLOWED_HOSTS = ["cooperative-enjoyment-staging.up.railway.app", "https://cooperative-enjoyment-staging.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://cooperative-enjoyment-staging.up.railway.app"]
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+
