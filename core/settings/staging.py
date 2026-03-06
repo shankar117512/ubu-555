791a3a4fb9a0c2f9a9a100cc800f33ab40537a05
shankar117512 <shankar117512@gmail.com>
2026-03-06 07:23:22 +0000

    staging.py

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -1,6 +1,6 @@
 from .base import *
 
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["cooperative-enjoyment-staging.up.railway.app", "https://cooperative-enjoyment-staging.up.railway.app"]
 CSRF_TRUSTED_ORIGINS = ["https://cooperative-enjoyment-staging.up.railway.app"]
 
